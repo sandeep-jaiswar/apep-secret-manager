6ec3e1c0a806affa37b70c3c0a85cfcf1d23b4ba
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2024-06-16 22:50:41 +0530

    feat: added tyoe safety to configmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { SecretModule } from './secret/secret.module';
 
+import * as Joi from '@hapi/joi';
+
 @Module({
-  imports: [SecretModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    SecretModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
